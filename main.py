2a143b201e8a45ce3b668e923dd9fe44adb1be8c
sandsmaster <chichkoliobopitko@gmail.com>
2025-09-15 15:24:29 +0200

    add def main and get_book_text

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text (url):
+    with open(url, "rt") as f:
+        return f.read()
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
