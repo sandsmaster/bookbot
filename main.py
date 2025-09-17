15e3af5dadcde216fea7d183b521418fea30f327
sandsmaster <chichkoliobopitko@gmail.com>
2025-09-17 17:47:27 +0200

    add sort_char

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-from stats import wc, char_stats
+from stats import wc, char_stats, sort_char
 
 def get_book_text (url):
     with open(url, "rt") as f:
@@ -9,6 +9,6 @@ def main():
     book_content = get_book_text("books/frankenstein.txt")
     num_words = wc(book_content)
     print(f"{num_words} words found in the document")
-    print(char_stats(book_content))
+    print(sort_char(char_stats(book_content)))
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,12 @@ def char_stats(text):
             stats_dict[c] = 1
     
     return stats_dict
+
+
+def sort_char(char_stats):
+    char_dicts = []
+    for char in char_stats:
+        if char.isalpha():
+            char_dicts.append({"char": char, "num": char_stats[char]})
+    char_dicts.sort(key=lambda x: x["num"], reverse=True)
+    return char_dicts
